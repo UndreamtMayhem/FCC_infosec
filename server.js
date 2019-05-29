349c96491fb1458fd3b96979f6df4887470a3ea5
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-05-29 21:49:09 +0100

    added passport

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,7 +12,17 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
+
+app.set('view engine', 'pug')
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route('/')
     .get((req, res) => {
         res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
