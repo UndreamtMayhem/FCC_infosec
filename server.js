c3c0b7a98b72bdc23128f5b2c67331616427e73a
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-05-29 23:14:19 +0100

    added login and register route

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const LocalStrategy = require('passport-local');
 
 
 require('dotenv').config();
-
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -59,9 +58,18 @@ mongo.connect(process.env.DATABASE, (err, db) => {
             }
         ));
 
+
+        function ensureAuthenticated(req, res, next) {
+            if (req.isAuthenticated()) {
+                return next();
+            }
+            res.redirect('/');
+        };
+
+
         app.route('/')
             .get((req, res) => {
-                res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'login', showLogin: true });
+                res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'login', showLogin: true, showRegistration: true });
             });
 
         app.route('/login')
@@ -70,9 +78,50 @@ mongo.connect(process.env.DATABASE, (err, db) => {
             });
 
         app.route('/profile')
-            .get((req, res) => {
-                res.render(process.cwd() + '/views/pug/profile');
+            .get(ensureAuthenticated, (req, res) => {
+                res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
             });
+
+        app.route('/register')
+            .post((req, res, next) => {
+                    db.collection('users').findOne({ username: req.body.username }, function(err, user) {
+                        if (err) {
+                            next(err);
+                        } else if (user) {
+                            res.redirect('/');
+                        } else {
+                            db.collection('users').insertOne({
+                                    username: req.body.username,
+                                    password: req.body.password
+                                },
+                                (err, doc) => {
+                                    if (err) {
+                                        res.redirect('/');
+                                    } else {
+                                        next(null, user);
+                                    }
+                                }
+                            )
+                        }
+                    })
+                },
+                passport.authenticate('local', { failureRedirect: '/' }),
+                (req, res, next) => {
+                    res.redirect('/profile');
+                }
+            );
+
+        app.route('/logout')
+            .get((req, res) => {
+                req.logout();
+                res.redirect('/');
+            });
+
+        app.use((req, res, next) => {
+            res.status(404)
+                .type('text')
+                .send('Not Found');
+        });
 
         app.listen(process.env.PORT || 3000, () => {
             console.log("Listening on port " + process.env.PORT);
