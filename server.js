ff4c58f8e2b3f19f501b4e37a7d70d4799b7ddba
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-05-29 21:32:56 +0100

    added pug

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
@@ -10,12 +10,12 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set('view engine', 'pug');
 app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    .get((req, res) => {
+        res.render('./pug/index.pug');
+    });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    console.log("Listening on port " + process.env.PORT);
 });
