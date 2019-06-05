f115591960425191a7269564a20b34d54f27ef6b
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-06-05 17:29:42 +0100

    added more views and routes
    basic routes finished metric and imperial to be created next and then hookup api route

--- Projects/metric-Imperial-converter/routes/routes.js
+++ Projects/metric-Imperial-converter/routes/routes.js
@@ -1,14 +1,58 @@
 module.exports = function(app, db) {
     app.route('/')
         .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index', {
-                title: 'Hello',
+            res.render(process.cwd() + '/views/pug/pages/index', {
+                title: 'Metric to Imperial Converter',
+
+            });
+        });
+
+    app.route('/api')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/api', {
+                title: 'API',
+
+            });
+        });
+
+    app.route('/capacity')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/capacity', {
+                title: 'Capacity',
 
             });
         });
 
 
+    app.route('/imperial')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/imperial', {
+                title: 'Imperial',
 
+            });
+        });
+    app.route('/length')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/length', {
+                title: 'Length',
+
+            });
+        });
+
+    app.route('/metric')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/metric', {
+                title: 'Metric',
+
+            });
+        });
+    app.route('/weight')
+        .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/pages/weight', {
+                title: 'Weight',
+
+            });
+        });
     app.use((req, res, next) => {
         res.status(404)
             .type('text')
