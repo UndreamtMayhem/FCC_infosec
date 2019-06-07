b6dd2ef0a18ee3220849c3f15cb2a9eebd5af27f
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-06-07 23:27:25 +0100

    updated form submit with validation checking
    improved form updating correct and incorrect values

--- Projects/metric-Imperial-converter/public/main.js
+++ Projects/metric-Imperial-converter/public/main.js
@@ -18,39 +18,55 @@ $(document)
             .sidebar('attach events', '.toc.item');
 
 
-
+        // refactor into generic form submit
         $('#ctof').click(function(e) {
 
             e.preventDefault();
+
             var form = $(this).parent();
+
             var celsiusValue = form.get(0).firstChild.firstChild;
+            var santizedData = $(celsiusValue).val().trim()
+
+
+            function isDataValid(value) {
+
+                /* data isnt empty */
+                if (value.length >= 1) {
+
+                    if (!isNaN(value)) return true; /* is data a number */
+
+                } else return false; /* data is empty */
+            }
 
             // check for empty input
-            if (!isNaN($(celsiusValue).val())) {
+            if (isDataValid(santizedData)) {
+
                 var displayValue = form.find("p");
 
                 $.post("/api/ctof", {
                     celsius: $(celsiusValue).val(),
 
                 }).done(function(data) {
-                    displayValue.text($(celsiusValue).val() + "°C to " + data.Farenheit + " °F")
+                    displayValue.text($(celsiusValue).val() + "°C to " + data.Farenheit + " °F");
+                    // repetitive code refactor
+                    var fieldToAddErrorClass = form.get(0).firstChild;
+                    $(fieldToAddErrorClass).removeClass("error");
+                    form.find(".ui.error.message").css("display", "none");
+
                 }).fail(function(data) {
                     // reuse this message
                     console.log(JSON.stringify(data.responseJSON))
                 })
 
             } else {
+
                 // add field error 
                 var fieldToAddErrorClass = form.get(0).firstChild;
                 $(fieldToAddErrorClass).addClass("error");
-
-
-
-
                 form.find(".ui.error.message").css("display", "block")
 
 
             }
-
         })
     });
