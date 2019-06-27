8b21168c9e83a6c47bb16894217b3407d562cb5a
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-06-27 22:36:07 +0100

    test for meter 2 feet and vice versa completed
    Written test for both meter to feet and feet to meter. all 318 test cases pass.
    Daniel Johnson

--- Projects/metric-Imperial-converter/test/LengthConversionTest.js
+++ Projects/metric-Imperial-converter/test/LengthConversionTest.js
@@ -4,6 +4,7 @@ var assert = chai.assert;
 var Length = require('../lib/length');
 var length = new Length();
 
+
 describe('Length conversion meterTofeet', function() {
 
     describe('testing zero value', function() {
@@ -47,11 +48,63 @@ describe('Length conversion meterTofeet', function() {
         var testValues = length.meterTofeetTestValues();
 
         for (var i in testValues) {
-            it(`kph ${i}  = ${testValues[i]}`, function() {
+            it(`meter ${i}  = feet ${testValues[i]}`, function() {
                 assert.equal(length.meterTofeet(i), testValues[i], 'incorrect conversion ');
             });
         }
     });
 
+});
 
+
+describe('Length conversion feetToMeter', function() {
+
+    describe('testing zero value', function() {
+        it('using single value e.g 0 feet should return 0 meter', function() {
+            assert.equal(length.feetToMeter(0), 0, ' 0 feet = 0 meter ');
+        });
+        it('using to 2 decimal places e.g 0.00 feet should return 0 meter', function() {
+            assert.equal(length.feetToMeter(0.00), 0, ' 0 feet = 0 meter ');
+        });
+        it('using to 3 decimal places e.g 0.000 feet should return 0 meter', function() {
+            assert.equal(length.feetToMeter(0.000), 0, ' 0 feet = 0 meter ');
+        });
+    });
+
+    describe('testing decimal precision', function() {
+        it('using single value e.g 1 feet should return 0.30 meter', function() {
+            assert.equal(length.feetToMeter(1), 0.30, ' 1 feet = 0.30 meter ');
+        });
+        it('using to 2 decimal places e.g 1.00 feet should return 0.30 feet', function() {
+            assert.equal(length.feetToMeter(1.00), 0.30, ' 1 feet = 0.30 meter ');
+        });
+        it('using to 3 decimal places e.g 1.000 feet should return 0.30 feet', function() {
+            assert.equal(length.feetToMeter(1.000), 0.30, ' 1 feet = 0.30 meter ');
+        });
+    });
+
+    describe('testing negative decimal precision', function() {
+        assert.equal(length.feetToMeter(-1.00), -0.30, ' -1 feet = -0.30 meter ');
+        it('using single value e.g -1 feet should return -0.30 meter', function() {
+            assert.equal(length.feetToMeter(-1), -0.30, ' -1 feet = -0.30 meter ');
+        });
+        it('using to 2 decimal places e.g -1.00 feet should return -0.30 feet', function() {
+            assert.equal(length.feetToMeter(-1.00), -0.30, ' -1 feet = -0.30 meter ');
+        });
+        it('using to 3 decimal places e.g -1.000 feet should return 3.28 feet', function() {
+            assert.equal(length.feetToMeter(-1.000), -0.30, ' -1 feet = -0.30 meter ');
+        });
+
+    });
+
+    describe('testing correctness', function() {
+
+        var testValues = length.feetToMeterTestValues();
+
+        for (var i in testValues) {
+            it(`feet ${i}  = meter ${testValues[i]}`, function() {
+                assert.equal(length.feetToMeter(i), testValues[i], 'incorrect conversion ');
+            });
+        }
+    });
 });
