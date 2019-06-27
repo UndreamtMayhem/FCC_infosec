d934c27bcca6f798f9d42bc2037822e0c80ae7e5
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-06-27 19:47:08 +0100

    Update LengthConversionTest.js
    added test to check zero values return 0
    Daniel Johnson

--- Projects/metric-Imperial-converter/test/LengthConversionTest.js
+++ Projects/metric-Imperial-converter/test/LengthConversionTest.js
@@ -5,7 +5,17 @@ var Length = require('../lib/length');
 var length = new Length();
 
 describe('Length conversion meterTofeet', function() {
-    console.log(length.meterTofeet(1));
 
+    describe('testing zero value', function() {
+        it('using single value e.g 0 should return 0 mph', function() {
+            assert.equal(length.meterTofeet(0), 0, ' 0 m = 0 feet ');
+        });
+        it('using to 2 decimal places e.g 0.00 should return 0 feet', function() {
+            assert.equal(length.meterTofeet(0.00), 0, ' 0 m = 0 feet ');
+        });
+        it('using to 3 decimal places e.g 0.000 should return 0 feet', function() {
+            assert.equal(length.meterTofeet(0.000), 0, ' 0 m = 0 feet ');
+        });
+    });
 
 });
